18d5f8367944c156c946616380d1fbf4bf381778
Juraci Vieira <juraci.vieira@gmail.com>
2017-05-30 21:44:29 -0300

    verse(3) returns the the verse

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,11 +2,18 @@ require_relative '../lib/bottles'
 
 describe '99 Bottles' do
   describe '#verse' do
-    it 'verse(99) returns the first verse' do
+    it 'returns the 99 verse' do
       expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
         "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
       expect(::Bottles.new.verse(99)).to eq(expected)
+    end
+
+    it 'returns the 3 verse' do
+      expected = "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+        "Take one down and pass it around, 2 bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(3)).to eq(expected)
     end
   end
 end
